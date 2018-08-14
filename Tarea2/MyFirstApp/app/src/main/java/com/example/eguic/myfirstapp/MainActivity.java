b9ede8749cf2773e22039ab358f771ddf6993df1
Erick Cordero Rojas <eguicoro2@gmail.com>
2018-08-14 15:19:53 -0600

    Tarea 2

--- Tarea2/MyFirstApp/app/src/main/java/com/example/eguic/myfirstapp/MainActivity.java
+++ Tarea2/MyFirstApp/app/src/main/java/com/example/eguic/myfirstapp/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.eguic.myfirstapp;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,15 +17,23 @@ import android.widget.EditText;
 import android.content.Intent;
 import android.widget.TextView;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
 public class MainActivity extends AppCompatActivity {
 
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     public String mvalor;
     public SharedPreferences mprefs;
+    public String tec;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+
         //print de prueba
         Log.d(" CREATION","onCreate");
         setContentView(R.layout.activity_main);
@@ -28,6 +43,9 @@ public class MainActivity extends AppCompatActivity {
 
         ((TextView)findViewById(R.id.textView4)).setText(
                 getString(R.string.activity_main_text_view_greeting, "Erick","Cordero"));
+
+        tec = createImageOnSDCard(R.drawable.tec);
+
     }
 
     @Override
@@ -95,4 +113,40 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
+
+    public void shareImage(View view){
+        Uri path = FileProvider.getUriForFile(this, "com.example.eguic.myfirstapp", new File(tec));
+
+        Intent shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.putExtra(Intent.EXTRA_TEXT, "This is one image I'm sharing.");
+        shareIntent.putExtra(Intent.EXTRA_STREAM, path);
+        shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        shareIntent.setType("image/*");
+        startActivity(Intent.createChooser(shareIntent, "Share..."));
+
+    }
+
+    /**
+     * Create an image on the phone's SD card to later be able to share it.
+     *
+     * @param resID resource ID of an image coming from the res folder.
+     * @return Return the path of the image that was created on the phone SD card.
+     */
+    private String createImageOnSDCard(int resID) {
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), resID);
+        String path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) + "/" + resID + ".jpg";
+        File file = new File(path);
+        try {
+            OutputStream out = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+            out.flush();
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return file.getPath();
+    }
+
 }
--- Tarea2/MySecondApp/app/src/main/java/com/example/eguic/mysecondapp/MainActivity.java
+++ Tarea2/MySecondApp/app/src/main/java/com/example/eguic/mysecondapp/MainActivity.java
@@ -1,7 +1,18 @@
 package com.example.eguic.mysecondapp;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +20,59 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        String type = intent.getType();
+
+
+        if (Intent.ACTION_SEND.equals(action) && type != null) {
+            if ("text/plain".equals(type)) {
+                handleSendText(intent); // Handle text being sent
+            } else if (type.startsWith("image/")) {
+                handleSendImage(intent); // Handle single image being sent
+            }
+        } else if (Intent.ACTION_SEND_MULTIPLE.equals(action) && type != null) {
+            if (type.startsWith("image/")) {
+                handleSendMultipleImages(intent); // Handle multiple images being sent
+            }
+        } else {
+            // Handle other intents, such as being started from the home screen
+        }
+    }
+
+    void handleSendText(Intent intent) {
+        String sharedText = intent.getStringExtra(Intent.EXTRA_TEXT);
+        if (sharedText != null) {
+            // Update UI to reflect text being shared
+        }
+    }
+
+    void handleSendImage(Intent intent)  {
+
+        //print de prueba
+        Log.d(" CREATION","entro el la funcion");
+
+        Uri imageUri = (Uri) intent.getParcelableExtra(Intent.EXTRA_STREAM);
+        if (imageUri != null) {
+            //print de prueba
+            Log.d(" CREATION","no es nulo");
+            // Update UI to reflect image being shared
+            ImageView img = findViewById(R.id.imageView);
+            Bitmap bitmap = null;
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            img.setImageBitmap(bitmap);
+        }
+    }
+
+    void handleSendMultipleImages(Intent intent) {
+        ArrayList<Uri> imageUris = intent.getParcelableArrayListExtra(Intent.EXTRA_STREAM);
+        if (imageUris != null) {
+            // Update UI to reflect multiple images being shared
+        }
     }
 }

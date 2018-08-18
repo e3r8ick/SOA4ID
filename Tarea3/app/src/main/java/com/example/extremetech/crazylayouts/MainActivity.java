6f4fb8a5f69520d7f2f9e20edc577395b3964d19
e3r8ick <eguicoro2@gmail.com>
2018-08-18 10:41:36 -0600

    Tarea3
    TArea sobre layouts

---
+++ Tarea3/app/src/main/java/com/example/extremetech/crazylayouts/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.extremetech.crazylayouts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ejemplo_grid_layout);
+    }
+}

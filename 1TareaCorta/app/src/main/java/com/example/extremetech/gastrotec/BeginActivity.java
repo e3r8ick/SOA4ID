d4bdb3c3dd044114fd2744a268a6be791920a256
Erick Cordero Rojas <eguicoro2@gmail.com>
2018-09-03 14:38:59 -0600

    cabios en el nav

--- 1TareaCorta/app/src/main/java/com/example/extremetech/gastrotec/BeginActivity.java
+++ 1TareaCorta/app/src/main/java/com/example/extremetech/gastrotec/BeginActivity.java
@@ -86,17 +86,13 @@ public class BeginActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_home) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_profile) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_config) {
 
         } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
 
         }
 

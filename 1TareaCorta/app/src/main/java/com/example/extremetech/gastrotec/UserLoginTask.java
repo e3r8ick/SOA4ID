20994adec5b465e88a663fcf39403b3bfd58da85
Erick Cordero Rojas <eguicoro2@gmail.com>
2018-09-15 15:57:59 -0600

    Proyecto 1

--- 1TareaCorta/app/src/main/java/com/example/extremetech/gastrotec/UserLoginTask.java
+++ 1TareaCorta/app/src/main/java/com/example/extremetech/gastrotec/UserLoginTask.java
@@ -1,6 +1,5 @@
 package com.example.extremetech.gastrotec;
 
-import android.content.Context;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.os.AsyncTask;
